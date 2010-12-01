a8297918014167e0cd687ef7cac7faf75cc1dcd9
Scott Kirkland <srkirkland@gmail.com>
2010-12-01 09:11:56 -0800

    added assembly info files, build files

---
+++ CommonAssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("University of California, Davis")]
+[assembly: AssemblyProduct("BuildVersionTest")]
+[assembly: AssemblyCopyright("Scott Kirkland & UC Regents")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTrademark("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.2.3.4")] //Will be replaced
+
+[assembly: NeutralResourcesLanguage("en-US")]
